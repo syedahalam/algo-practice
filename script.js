a7d9b4a3d85b540cf9594b6226069b7238985c8b
Syeda Alam <syedahalam@yahoo.com>
2022-06-25 01:44:24 -0400

    reverse

--- script.js
+++ script.js
@@ -2,7 +2,18 @@
 // 	const arr = str.split('').reverse().join('');
 // 	return arr;
 // };
-const reverse = (str) => str.split('').reverse().join('');
-console.log(reverse('apple'));
+// const reverse = (str) => str.split('').reverse().join('');
+// console.log(reverse('apple'));
 
 // console.log('hello');
+
+// //////// palindrome ///////
+
+const palindrome = (str) => {
+	const arr = str.split('').reverse().join('');
+	if (arr === str) return true;
+	else return false;
+};
+console.log(palindrome('apple'));
+console.log(palindrome('abba'));
+console.log(palindrome('a'));
