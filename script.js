87cc5ca17ef1ad5d5326fc878272cbff692c38a4
Syeda Alam <syedahalam@yahoo.com>
2022-06-25 01:26:22 -0400

    reverse

---
+++ script.js
@@ -0,0 +1,8 @@
+// const reverse = (str) => {
+// 	const arr = str.split('').reverse().join('');
+// 	return arr;
+// };
+const reverse = (str) => str.split('').reverse().join('');
+console.log(reverse('apple'));
+
+// console.log('hello');
