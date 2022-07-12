19b1c27e15c5f15ad201ab90868503c1b38655bf
Syeda Alam <syedahalam@yahoo.com>
2022-07-12 10:51:29 -0400

    start practice

--- script.js
+++ script.js
@@ -9,11 +9,22 @@
 
 // //////// palindrome ///////
 
-const palindrome = (str) => {
+// const palindrome = (str) => {
+// 	const arr = str.split('').reverse().join('');
+// 	if (arr === str) return true;
+// 	else return false;
+// };
+// console.log(palindrome('apple'));
+// console.log(palindrome('abba'));
+// console.log(palindrome('a'));
+
+// //////////// reverse int//////////
+const pali = (str) => {
 	const arr = str.split('').reverse().join('');
-	if (arr === str) return true;
+	if (str === arr) return true;
 	else return false;
 };
-console.log(palindrome('apple'));
-console.log(palindrome('abba'));
-console.log(palindrome('a'));
+console.log(pali('humna'));
+console.log(pali('abc'));
+console.log(pali('12321'));
+console.log(pali('abba'));
