cbabbfff2156f2f9fec36e0de888c7d4713a9de1
Syeda Alam <syedahalam@yahoo.com>
2022-07-13 15:32:08 -0400

    practice complete

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const chunk = (array, size) => {
 	const chunked = [];
+
 	for (let element of array) {
-		const last = chunked[chu];
+		const last = chunked[chunked.length - 1];
+		if (!last || last.length === size) {
+			chunked.push([element]);
+		} else {
+			last.push(element);
+		}
 	}
+	return chunked;
 };
+console.log(chunk([1, 2, 3, 4, 5, 6], 3));
