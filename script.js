9e6071d6911332510a5b452f71a8ad12b4c26a77
Syeda Alam <syedahalam@yahoo.com>
2022-07-12 23:05:15 -0400

    max char

--- script.js
+++ script.js
@@ -1,42 +1,21 @@
-// const reverse = (str) => {
-// 	const arr = str.split('').reverse().join('');
-// 	return arr;
-// };
-// const reverse = (str) => str.split('').reverse().join('');
-// console.log(reverse('apple'));
-
-// console.log('hello');
-
-// //////// palindrome ///////
-
-// const palindrome = (str) => {
-// 	const arr = str.split('').reverse().join('');
-// 	if (arr === str) return true;
-// 	else return false;
-// };
-// console.log(palindrome('apple'));
-// console.log(palindrome('abba'));
-// console.log(palindrome('a'));
-
-// //////////// reverse int//////////
-// const revInt = (int) => {
-// 	const stt = String(int);
-// 	const abb = Number(stt.split('').reverse().join(''));
-// 	return abb;
-// };
-
-const revInt = (int) => {
-	const abb = int.toString().split('').reverse().join('');
-	const zxx = parseInt(abb);
-	if (int < 0) {
-		return zxx * -1;
-	} else {
-		return zxx;
+const maxChar = (str) => {
+	const charMap = {};
+	let max = 0;
+	let maxChar = '';
+	for (let char of str) {
+		if (charMap[char]) {
+			charMap[char]++;
+		} else {
+			charMap[char] = 1;
+		}
 	}
+	for (let char in charMap) {
+		if (charMap[char] > max) {
+			max = charMap[char];
+			maxChar = char;
+		}
+	}
+	return maxChar;
 };
-
-console.log(revInt(15));
-console.log(revInt(981));
-console.log(revInt(500));
-console.log(revInt(-15));
-console.log(revInt(-90));
+console.log(maxChar('hello'));
+console.log(maxChar('hello humna hi mommy'));
