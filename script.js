04a6705202126b6b428fb55cabb7566d9191ac81
Syeda Alam <syedahalam@yahoo.com>
2022-07-18 15:11:18 -0400

    revising

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
-// reverse
-
 const rev = (str) => str.split('').reverse().join('');
 // console.log(rev('hello'));
-
-// palindrome
 
 const palindrome = (str) => {
 	const aqq = str.split('').reverse().join('');
 	return str === aqq;
 };
+// console.log(palindrome('hello'));
 // console.log(palindrome('abba'));
-// console.log(palindrome('humna'));
-
-// reverse int
 
 const revInt = (n) => {
 	const aqq = n.toString().split('').reverse().join('');
@@ -21,27 +15,22 @@ const revInt = (n) => {
 // console.log(revInt(15));
 // console.log(revInt(-15));
 
-// maximum characters
-
-const maxChar = (str) => {
+const charMax = (str) => {
 	const charMap = {};
 	let max = 0;
-	let maxChar = '';
+	let charMax;
 	for (let char of str) {
 		charMap[char] = charMap[char] + 1 || 1;
 	}
 	for (let char in charMap) {
 		if (charMap[char] > max) {
 			max = charMap[char];
-			maxChar = char;
+			charMax = char;
 		}
 	}
-	return maxChar;
+	return charMax;
 };
-// console.log(maxChar('hello'));
-// console.log(maxChar('12222233445'));
-
-// chunk array
+// console.log(charMax('hellloooo'));
 
 const chunkArray = (array, size) => {
 	const chunked = [];
@@ -55,11 +44,9 @@ const chunkArray = (array, size) => {
 	}
 	return chunked;
 };
-// console.log(chunkArray([1, 2, 3, 4, 5, 6, 7, 8], 3));
+// console.log(chunkArray([1, 2, 3, 4, 5], 3));
 
-// anagrams
-
-const anagrams = (stringA, stringB) => {
+const anagram = (stringA, stringB) => {
 	const aCharMap = buildMap(stringA);
 	const bCharMap = buildMap(stringB);
 
@@ -80,17 +67,13 @@ const buildMap = (str) => {
 	}
 	return charMap;
 };
-// console.log(anagrams('hello', 'hello'));
+// console.log(anagram('hello', 'hell'));
 
-const anagram = (stringA, stringB) => {
+const anagram1 = (stringA, stringB) => {
 	return cleanUp(stringA) === cleanUp(stringB);
 };
-const cleanUp = (str) => {
-	return str.replace(/[\^w]/g, '').toLowerCase().split('').sort().join('');
-};
-// console.log(anagram('hello', 'hello'));
-
-// fizzbuzz
+const cleanUp = (str) => str.replace(/[\^w]/g, '').split('').sort().join('');
+// console.log(anagram1('hello', 'hello'));
 
 // const fizzBuzz = (n) => {
 // 	for (let i = 1; i <= n; i++) {
@@ -107,39 +90,32 @@ const cleanUp = (str) => {
 // };
 // fizzBuzz(15);
 
-// capitalization
-
 const capitalization = (str) => {
-	const words = [];
+	let words = [];
 	for (let word of str.split(' ')) {
 		words.push(word[0].toUpperCase() + word.slice(1));
 	}
 	return words.join(' ');
 };
-// console.log(capitalization('hi humna how are you?'));
+// console.log(capitalization('hi there'));
 
-// steps
-
-// const steps = (n) => {
-// 	for (let rows = 0; rows < n; rows++) {
-// 		let stairs = '';
-// 		for (let columns = 0; columns < n; columns++) {
-// 			if (columns <= rows) {
-// 				stairs += '#';
-// 			} else {
-// 				stairs += ' ';
-// 			}
-// 		}
-// 		console.log(stairs);
-// 	}
-// };
-// steps(5);
-
-// pyramid
+const steps = (n) => {
+	for (let rows = 0; rows < n; rows++) {
+		let stairs = '';
+		for (let columns = 0; columns < n; columns++) {
+			if (columns <= rows) {
+				stairs += '#';
+			} else {
+				stairs += ' ';
+			}
+		}
+		console.log(stairs);
+	}
+};
+steps(6);
 
 const pyramid = (n) => {
-	const midpoint = Math.floor((2 * n - 1) / 2);
-
+	let midpoint = Math.floor((2 * n - 1) / 2);
 	for (let rows = 0; rows < n; rows++) {
 		let levels = '';
 		for (let columns = 0; columns < 2 * n - 1; columns++) {
@@ -152,4 +128,4 @@ const pyramid = (n) => {
 		console.log(levels);
 	}
 };
-// pyramid(6);
+pyramid(6);
