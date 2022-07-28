4b7c9d25794226f384dd1b6b7ef8a7e986020f73
Syeda Alam <syedahalam@yahoo.com>
2022-07-28 17:55:27 -0400

    ll

--- script.js
+++ script.js
@@ -1,5 +1,45 @@
+class Node {
+	constructor(data, next = null) {
+		this.data = data;
+		this.next = next;
+	}
+}
 class LinkedList {
 	constructor() {
 		this.head = null;
 	}
+	insertFirst(data) {
+		this.head = new Node(data, this.head);
+	}
+	size() {
+		let counter = 0;
+		let node = this.head;
+		while (node) {
+			counter++;
+			node = node.next;
+		}
+		return counter;
+	}
+	getFirst() {
+		return this.head;
+	}
+	getLast() {
+		let node = this.head;
+		while (node) {
+			if (!node) {
+				return node;
+			}
+			node = node.next;
+		}
+	}
+	clear() {
+		this.head = null;
+	}
+	removeFirst() {
+		if (!this.head) {
+			return;
+		}
+		this.head = this.head.next;
+	}
+	removeLast() {}
 }
