dda08e49f722d7d944729df1932f52547bb35ad7
Syeda Alam <syedahalam@yahoo.com>
2022-07-30 00:22:56 -0400

    revising

--- script.js
+++ script.js
@@ -50,7 +50,6 @@ class LinkedList {
 		}
 		if (!this.head.next) {
 			this.head = null;
-			return;
 		}
 		let previous = this.head;
 		let node = this.head.next;
@@ -72,7 +71,7 @@ class LinkedList {
 		let counter = 0;
 		let node = this.head;
 		while (node) {
-			if (counter === index) {
+			if (index === counter) {
 				return node;
 			}
 			counter++;
@@ -80,11 +79,29 @@ class LinkedList {
 		}
 		return null;
 	}
-	removeAt() {
+	removeAt(index) {
 		if (!this.head) {
 			return;
 		}
 		if (index === 0) {
+			this.head = this.head.next;
 		}
+		let previous = this.getAt(index - 1);
+		if (!previous || !previous.next) {
+			return;
+		}
+		previous.next = previous.next.next;
+	}
+	insertAt(data, index) {
+		if (!this.head) {
+			this.head = new Node(data);
+			return;
+		}
+		if (index === 0) {
+			this.head = new Node(data, this.head);
+		}
+		const previous = this.getAt(index - 1) || this.getLast();
+		const node = new Node(data, previous.next);
+		previous.next = node;
 	}
 }
