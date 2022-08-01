038d83f6ab8139bb42a52184d844bd7cc58bdc2a
Syeda Alam <syedahalam@yahoo.com>
2022-08-01 18:15:07 -0400

    revising queue, stacks

--- script.js
+++ script.js
@@ -1,228 +1,83 @@
-const rev = (str) => str.split('').reverse().join('');
-// console.log(rev('hello'));
-
-const palindrome = (str) => {
-	const aqq = str.split('').reverse().join('');
-	return aqq === str;
-};
-// console.log(palindrome('abba'));
-
-const revInt = (n) => {
-	const aqq = n.toString().split('').reverse().join('');
-	return parseInt(aqq) * Math.sign(n);
-};
-// console.log(revInt(-15));
-
-const charMax = (str) => {
-	const charMap = {};
-	let max = 0;
-	let charMax = '';
-	for (let char of str.toLowerCase()) {
-		charMap[char] = charMap[char] + 1 || 1;
+class Queue {
+	constructor() {
+		this.data = [];
 	}
-	for (let char in charMap) {
-		if (charMap[char] > max) {
-			max = charMap[char];
-			charMax = char;
+	add(record) {
+		this.data.unshift(record);
+	}
+	remove() {
+		return this.data.pop();
+	}
+}
+class Queue {
+	constructor() {
+		this.data = [];
+	}
+	add(record) {
+		this.data.unshift(record);
+	}
+	remove() {
+		return this.data.pop();
+	}
+	peek() {
+		return this.data[this.data.length - 1];
+	}
+}
+
+const weave = (sourceOne, sourceTwo) => {
+	const q = new Queue();
+	while (sourceOne.peek() || sourceTwo.peek()) {
+		if (sourceOne.peek()) {
+			q.add(sourceOne.remove());
+		}
+		if (sourceTwo.peek()) {
+			q.add(sourceTwo.remove());
 		}
 	}
-	return charMax;
+	return q;
 };
-// console.log(charMax('hello1111222222266666666666666 '));
+class Stack {
+	constructor() {
+		this.data = [];
+	}
+	add(record) {
+		this.data.push(record);
+	}
+	remove() {
+		return this.data.pop();
+	}
+	peek() {
+		return this.data[this.data.length - 1];
+	}
+}
 
-const chunkArray = (array, size) => {
-	const chunked = [];
-	for (let element of array) {
-		const last = chunked[chunked.length - 1];
-		if (!last || last.length === size) {
-			chunked.push([element]);
-		} else {
-			last.push(element);
+class Queue {
+	constructor() {
+		this.first = new Stack();
+		this.second = new Stack();
+	}
+	add(record) {
+		this.first.push(record);
+	}
+	remove() {
+		while (this.first.peek()) {
+			this.second.push(this.first.pop());
 		}
-	}
-	return chunked;
-};
-// console.log(chunkArray([1, 2, 3, 4, 5, 6, 7, 8, 9], 3));
+		const record = this.second.pop;
 
-// const anagram = (stringA, stringB) => {
-// 	return cleanUp(stringA) === cleanUp(stringB);
-// };
-// const cleanUp = (str) => {
-// 	return str
-// 		.replace(/[\^w]/g / '')
-// 		.toLowerCase()
-// 		.split('')
-// 		.sort()
-// 		.join('');
-// };
-// console.log(anagram('hello', 'hell'));
-
-const anagram = (stringA, stringB) => {
-	const aCharMap = buildMap(stringA);
-	const bCharMap = buildMap(stringB);
-
-	if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
-		return false;
-	}
-	for (let char in aCharMap) {
-		if (aCharMap[char] !== bCharMap[char]) {
-			return false;
+		while (this.second.peek()) {
+			this.first.push(this.second.pop);
 		}
+		return record;
 	}
-	return true;
-};
-const buildMap = (str) => {
-	const charMap = {};
-	for (let char of str.toLowerCase()) {
-		charMap[char] = charMap[char] + 1 || 1;
-	}
-	return charMap;
-};
-// console.log(anagram('hello', 'hell'));
-
-// const fizzBuzz = (n) => {
-// 	for (let i = 0; i <= n; i++) {
-// 		if (i % 15 === 0) {
-// 			console.log('fizzbuzz');
-// 		} else if (i % 3 === 0) {
-// 			console.log('fizz');
-// 		} else if (i % 5 === 0) {
-// 			console.log('buzz');
-// 		} else {
-// 			console.log(i);
-// 		}
-// 	}
-// };
-// fizzBuzz(15);
-const capitalization = (str) => {
-	let words = [];
-	for (let word of str.split(' ')) {
-		words.push(word[0].toUpperCase() + word.slice(1));
-	}
-	return words.join(' ');
-};
-
-// console.log(capitalization('hi humna! how are you?'));
-
-// const steps = (n) => {
-// 	for (let rows = 0; rows <= n; rows++) {
-// 		let stairs = '';
-// 		for (columns = 0; columns <= n; columns++) {
-// 			if (columns < rows) {
-// 				stairs += '#';
-// 			} else {
-// 				stairs += ' ';
-// 			}
-// 		}
-// 		console.log(stairs);
-// 	}
-// };
-// steps(6);
-
-// const pyramid = (n) => {
-// 	let midpoint = Math.floor((2 * n - 1) / 2);
-// 	for (let rows = 0; rows < n; rows++) {
-// 		let levels = '';
-// 		for (let columns = 0; columns < 2 * n - 1; columns++) {
-// 			if (midpoint - rows <= columns && midpoint + rows >= columns) {
-// 				levels += '#';
-// 			} else {
-// 				levels += ' ';
-// 			}
-// 		}
-// 		console.log(levels);
-// 	}
-// };
-// pyramid(6);
-
-// const vowels = (str) => {
-// 	const matches = str.match(/[aeiou]/gi);
-// 	return matches ? matches.length : 0;
-// };
-
-const vowels = (str) => {
-	let counter = 0;
-	let letters = ['a', 'e', 'i', 'o', 'u'];
-	for (let char of str.toLowerCase()) {
-		if (letters.includes(char)) {
-			counter++;
+	peek() {
+		while (this.first.peek()) {
+			this.second.push(this.first.pop());
 		}
+		const record = this.second.peek();
+		while (this.second.peek()) {
+			this.first.push(this.second.pop());
+		}
+		return record;
 	}
-	return counter;
-};
-// console.log(vowels('humiona'));
-
-// const matrix = (n) => {
-// 	let result = [];
-// 	for (i = 0; i < n; i++) {
-// 		result.push([]);
-// 	}
-// 	let counter = 1;
-// 	let startRow = 0;
-// 	let endRow = n - 1;
-// 	let startColumn = 0;
-// 	let endColumn = n - 1;
-// 	while (startRow <= endRow && startColumn <= endColumn) {
-// 		//    top row
-// 		for (let i = startColumn; i <= endColumn; i++) {
-// 			result[startRow][i] = counter;
-// 			counter++;
-// 		}
-// 		startRow++;
-
-// 		//  right column
-
-// 		for (i = startRow; i <= endRow; i++) {
-// 			result[i][endColumn] = counter;
-// 			counter++;
-// 		}
-// 		endColumn--;
-
-// 		// bottom row
-
-// 		for (let i = endColumn; i >= startColumn; i--) {
-// 			result[endRow][i] = counter;
-// 			counter++;
-// 		}
-// 		endRow--;
-
-// 		// left column
-// 		for (let i = endRow; i >= startRow; i--) {
-// 			result[i][startColumn] = counter;
-// 			counter++;
-// 		}
-// 		startColumn++;
-// 	}
-// 	return result;
-// };
-// console.log(matrix(6));
-
-// const fib = (n) => {
-// 	const array = [0, 1];
-
-// 	for (let i = 2; i <= n; i++) {
-// 		const a = array[i - 1];
-// 		const b = array[i - 2];
-// 		array.push(a + b);
-// 	}
-// 	return array[n];
-// };
-// console.log(fib(6));
-
-// const memoization = (fib) => {
-// 	const cache = {};
-
-// 	return (...args) => {
-// 		if (cache[args]) {
-// 			return cache[args];
-// 		}
-// 		const record = fib.apply(this, args);
-// 		return record;
-// 	};
-// };
-// const slowfib = (n) => {
-// 	if (n < 2) return n;
-// 	else return slowfib(n - 1) + slowfib(n - 2);
-// };
-// const fib = memoization(slowfib);
-// console.log(fib(4));
+}
