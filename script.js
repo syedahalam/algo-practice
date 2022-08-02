6bab4481ac958a3f069974ebca31a063ed9490f2
Syeda Alam <syedahalam@yahoo.com>
2022-08-01 21:55:33 -0400

    trees

--- script.js
+++ script.js
@@ -1,150 +1,52 @@
 class Node {
-	constructor(data, next = null) {
-		this.head = data;
-		this.next = next;
+	constructor(data) {
+		this.data = data;
+		this.children = [];
+	}
+	add(data) {
+		this.children.push(new Node(data));
+	}
+	remove(data) {
+		this.children = this.children.filter((node) => {
+			return node.data !== data;
+		});
 	}
 }
-class LinkedList {
+class Tree {
 	constructor() {
-		this.head = null;
+		this.root = null;
 	}
-	insertFirst(data) {
-		const node = new Node(data);
-	}
-	size() {
-		let counter = 0;
-		let node = this.head;
-		while (node) {
-			counter++;
-			node = node.next;
-		}
-		return counter;
-	}
-	getFirst() {
-		return this.head;
-	}
-	getLast() {
-		if (!this.head) {
-			return false;
-		}
-		let node = this.head;
-		while (node) {
-			if (!node.next) {
-				return node;
-			}
-			node = node.next;
+	transverseBF(fn) {
+		const arr = [this.root];
+		while (arr.length) {
+			const node = arr.shift();
+			arr.push(...node.children);
+			fn(node);
 		}
 	}
-	clear() {
-		this.head = null;
-	}
-	removeFirst() {
-		if (!this.head) {
-			return;
+	transverseDF(fn) {
+		const arr = [this.root];
+		while (arr.length) {
+			const node = arr.shift();
+			arr.unshift(...node.children);
+			fn(node);
 		}
-		this.head = this.head.next;
-	}
-	removeLast() {
-		if (!this.head) {
-			return null;
-		}
-		if (!this.head.next) {
-			this.head = null;
-			return;
-		}
-		const prev = this.head;
-		const node = this.head.next;
-		while (node) {
-			prev = node;
-			node = node.next;
-		}
-		prev.next = null;
-	}
-	insertLast(data) {
-		const last = this.getLast();
-		if (last) {
-			last.next = new Node(data);
-		} else {
-			this.head = new Node(data);
-		}
-	}
-	getAt(index) {
-		let counter = 0;
-		let node = this.head;
-		while (node) {
-			if (counter === index) {
-				return node;
-			}
-			counter++;
-			node = node.next;
-		}
-		return null;
-	}
-	removeAt(index) {
-		if (!this.head) {
-			return null;
-		}
-		if (index === 0) {
-			this.head = this.head.next;
-			return;
-		}
-		const previous = this.getAt(index) - 1;
-		if (!previous || !previous.next) {
-			return;
-		}
-		previous.next = previous.next.next;
-	}
-	insertAt(data, index) {
-		if (!this.head) {
-			this.head = new Node(data);
-			return;
-		}
-		if (index === 0) {
-			this.head = new Node(data, this.head);
-			return;
-		}
-		const previous = this.getAt(index - 1);
-		const node = new Node(data, previous.next);
-		previous.next = node;
 	}
 }
-const midpoint = (LinkedList) => {
-	let slow = LinkedList.getFirst();
-	let fast = LinkedList.getFirst();
+const levelWidth = (root) => {
+	const arr = [root, 'check'];
+	const counter = [0];
 
-	while (fast.next && fast.next.next) {
-		slow = slow.next;
-		fast = fast.next.next;
-	}
-	return slow;
-};
+	while (arr.length) {
+		const node = arr.shift();
 
-const circularList = (LinkedList) => {
-	const slow = LinkedList.getFirst();
-	const fast = LinkedList.getFirst();
-
-	while (fast.next && fast.next.next) {
-		slow = slow.next;
-		fast = fast.next.next;
-
-		if (slow === fast) {
-			return true;
+		if (node === 'check') {
+			counter.push(0);
+			arr.push('check');
+		} else {
+			arr.push(...node.children);
+			counter[counter.length - 1]++;
 		}
 	}
-	return null;
-};
-
-const stepFromTail = (LinkedList, n) => {
-	const slow = LinkedList.getFirst();
-	const fast = LinkedList.getFirst();
-
-	while (n > 0) {
-		fast = fast.next;
-		n--;
-	}
-	while (fast.next) {
-		slow = slow.next;
-		fast = fast.next;
-	}
-	return slow;
+	return counter;
 };
