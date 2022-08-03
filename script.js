fe35ed02c0952538273b2dbdfb5e20119c26a5f9
Syeda Alam <syedahalam@yahoo.com>
2022-08-03 18:00:57 -0400

    finished

--- script.js
+++ script.js
@@ -1,52 +1,56 @@
-class Node {
-	constructor(data) {
-		this.data = data;
-		this.children = [];
-	}
-	add(data) {
-		this.children.push(new Node(data));
-	}
-	remove(data) {
-		this.children = this.children.filter((node) => {
-			return node.data !== data;
-		});
-	}
-}
-class Tree {
-	constructor() {
-		this.root = null;
-	}
-	transverseBF(fn) {
-		const arr = [this.root];
-		while (arr.length) {
-			const node = arr.shift();
-			arr.push(...node.children);
-			fn(node);
+const bubbleSort = (arr) => {
+	for (let i = 0; i < arr.length; i++) {
+		for (let j = 0; j < arr.length - i - 1; j++) {
+			if (arr[j] > arr[j + 1]) {
+				const lesser = arr[j + 1];
+				arr[j + 1] = arr[j];
+				arr[j] = lesser;
+			}
 		}
 	}
-	transverseDF(fn) {
-		const arr = [this.root];
-		while (arr.length) {
-			const node = arr.shift();
-			arr.unshift(...node.children);
-			fn(node);
-		}
-	}
-}
-const levelWidth = (root) => {
-	const arr = [root, 'check'];
-	const counter = [0];
-
-	while (arr.length) {
-		const node = arr.shift();
-
-		if (node === 'check') {
-			counter.push(0);
-			arr.push('check');
-		} else {
-			arr.push(...node.children);
-			counter[counter.length - 1]++;
-		}
-	}
-	return counter;
+	return arr;
 };
+// console.log(bubbleSort([23, 11, 6, 0]));
+
+const selectionSort = (arr) => {
+	for (let i = 0; i < arr.length; i++) {
+		let indexOfMin = i;
+
+		for (let j = i + 1; j < arr.length; j++) {
+			if (arr[j] < arr[indexOfMin]) {
+				indexOfMin = j;
+			}
+		}
+		if (indexOfMin !== i) {
+			let lesser = arr[indexOfMin];
+			arr[indexOfMin] = arr[i];
+			arr[i] = lesser;
+		}
+	}
+	return arr;
+};
+// console.log(selectionSort([23, 1, 56]));
+
+const mergeSort = (arr) => {
+	if (arr.length === 1) {
+		return arr;
+	}
+	const center = Math.floor(arr.length / 2);
+	const left = arr.slice(0, center);
+	const right = arr.slice(center);
+
+	return merge(mergeSort(left), mergeSort(right));
+};
+
+const merge = (left, right) => {
+	const results = [];
+	while (left.length && right.length) {
+		if (left[0] < right[0]) {
+			results.push(left.shift());
+		} else {
+			results.push(right.shift());
+		}
+	}
+	return [...results, ...left, ...right];
+};
+console.log(mergeSort([23, 1, 43, 7, 44]));
